00ccd881a711dafeaf34fa5177067e9e6f87283d
Abdulmalik Adebayo <olanrewajuabdulmalik42@gmail.com>
2026-09-16 12:27:04 +0100

    mount about&contact in sever

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(logger);
 
 app.use(express.json());
 
-const pages = ['dashboard', 'login', 'register', 'profile', 'results', 'admin', 'superadmin', 'soc'];
+const pages = ['dashboard', 'login', 'register', 'profile', 'results', 'admin', 'superadmin', 'soc','about', 'contact'];
 pages.forEach(page => {
     app.get(`/${page}`, (req, res) => {
         res.sendFile(path.join(__dirname, 'public', `${page}.html`));
